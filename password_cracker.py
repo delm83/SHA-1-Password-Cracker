278bc119d39e1e7d6041bc9eaab6265199ebcc9b
delm83 <delm83@googlemail.com>
2024-09-01 10:32:15 +0100

    passes all tests

--- password_cracker.py
+++ password_cracker.py
@@ -1,9 +1,18 @@
 import hashlib
-passwordlist = [line.rstrip('\n') for line in open('top-10000-passwords.txt', 'r')]
-saltlist = [line.rstrip('\n') for line in open('known-salts.txt', 'r')]
+passwordlist = [line.rstrip() for line in open('top-10000-passwords.txt', 'r')]
+saltlist = [line.rstrip() for line in open('known-salts.txt', 'r')]
 def crack_sha1_hash(hash, use_salts = False):
     for password in passwordlist:
-        if hash == hashlib.sha1(password.encode()).hexdigest():
-            return password
+        if use_salts:
+            for salt in saltlist:
+                saltedpassword = salt+password
+                if hash == hashlib.sha1(saltedpassword.encode()).hexdigest():
+                    return password
+                saltedpassword = password+salt
+                if hash == hashlib.sha1(saltedpassword.encode()).hexdigest():
+                    return password
+        else:
+            if hash == hashlib.sha1(password.encode()).hexdigest():
+                return password
 
     return "PASSWORD NOT IN DATABASE"
